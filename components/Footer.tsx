dd03cbb19ba2d3632a79e5102264c4815961a39a
Anurag Kumar <anuragabcr@gmail.com>
2023-08-08 09:14:48 +0530

    footer added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
+import Logo from "@/public/images/Logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box m="80px" bgcolor="#fff3f4" alignItems="center" alignContent="center">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <Image src={Logo} alt="logo" height={50} width={50} />
+        <Typography variant="h3">PulseFusion</Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
